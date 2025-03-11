9b33a17e15cf765518bbcf59ab2e0c45590cc035
Plubakkara <149078695+Plubakkara@users.noreply.github.com>
2025-03-11 21:07:50 +0700

    Update DogsVsCatsPredictor.jsx

--- frontend/src/pages/DogsVsCatsPredictor.jsx
+++ frontend/src/pages/DogsVsCatsPredictor.jsx
@@ -9,7 +9,7 @@ export default function DogsVsCatsPredictor() {
   const [error, setError] = useState(null);
 
   // ✅ กำหนดให้ใช้ Localhost เท่านั้น
-  const API_BASE_URL = "https://e94c-34-75-110-13.ngrok-free.app"; 
+  const API_BASE_URL = "https://506a-35-185-19-211.ngrok-free.app"; 
 
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
